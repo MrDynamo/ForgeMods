df609bba05105cb784a5e50c57fa92a68494c886
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-19 05:01:14 -0500

    v1.3
    Implemented build number variables

--- examplemod/src/main/java/com/github/mrdynamo/examplemod/ExampleMod.java
+++ examplemod/src/main/java/com/github/mrdynamo/examplemod/ExampleMod.java
@@ -1,26 +1,27 @@
 package com.github.mrdynamo.examplemod;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod("examplemod")
+@Mod(ExampleMod.MODID)
 public class ExampleMod
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    /*** Private class variables ***/
+    private IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+    /*** Public class variables ***/
+    public static final String MODID = "examplemod";
+
+    // Constructor
     public ExampleMod() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        modBus.addListener(this::setup);
         // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        modBus.addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
