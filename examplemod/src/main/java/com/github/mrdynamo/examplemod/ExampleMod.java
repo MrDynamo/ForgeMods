18f10af4476dcfc31470c32e9e6f8bfec7067b9a
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-19 02:54:52 -0500

    v1.2
    Cleaned up code and setup gradle tasks

--- examplemod/src/main/java/com/github/mrdynamo/examplemod/ExampleMod.java
+++ examplemod/src/main/java/com/github/mrdynamo/examplemod/ExampleMod.java
@@ -1,22 +1,14 @@
 package com.github.mrdynamo.examplemod;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 @Mod("examplemod")
 public class ExampleMod
@@ -43,10 +35,4 @@ public class ExampleMod
         // do something that can only be done on the client
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-    
 }
