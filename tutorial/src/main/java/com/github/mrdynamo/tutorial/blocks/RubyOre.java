05de498a29d03c9e952f586a8bd1d5429b330d45
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-19 19:17:59 -0500

    Episode 5
    Ruby ore and loot tables

---
+++ tutorial/src/main/java/com/github/mrdynamo/tutorial/blocks/RubyOre.java
@@ -0,0 +1,19 @@
+package com.github.mrdynamo.tutorial.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class RubyOre extends Block {
+
+    public RubyOre() {
+        super(Block.Properties.create(Material.IRON)
+                .hardnessAndResistance(5.0f, 6.0f)
+                .harvestLevel(2)
+                .harvestTool(ToolType.PICKAXE)
+                .sound(SoundType.METAL)
+        );
+    }
+
+}
