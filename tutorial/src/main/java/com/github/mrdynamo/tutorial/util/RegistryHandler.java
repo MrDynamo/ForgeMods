371b6e2435a21ec2922805561518ffa09ddd5d77
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-19 19:17:28 -0500

    Episode 5
    Ruby ore and loot tables

--- tutorial/src/main/java/com/github/mrdynamo/tutorial/util/RegistryHandler.java
+++ tutorial/src/main/java/com/github/mrdynamo/tutorial/util/RegistryHandler.java
@@ -3,6 +3,7 @@ package com.github.mrdynamo.tutorial.util;
 import com.github.mrdynamo.tutorial.Tutorial;
 import com.github.mrdynamo.tutorial.blocks.BlockItemBase;
 import com.github.mrdynamo.tutorial.blocks.RubyBlock;
+import com.github.mrdynamo.tutorial.blocks.RubyOre;
 import com.github.mrdynamo.tutorial.items.ItemBase;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -16,6 +17,7 @@ public class RegistryHandler {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Tutorial.MODID);
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Tutorial.MODID);
 
+    // Init mod assets-
     public static void init() {
         ITEMS.register(Tutorial.modBus);
         BLOCKS.register(Tutorial.modBus);
@@ -26,9 +28,10 @@ public class RegistryHandler {
 
     /*** Blocks ***/
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+    public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", RubyOre::new);
 
     /*** Block Items ***/
     public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
-
+    public static final RegistryObject<Item> RUBY_ORE_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_ORE.get()));
 
 }
