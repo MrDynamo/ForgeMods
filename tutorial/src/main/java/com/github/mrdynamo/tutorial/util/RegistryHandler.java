5133536ebbd74a7a9e8d989b2f0cb4ea8ca45d31
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-19 20:19:16 -0500

    Episode 7
    Custom mod tools

--- tutorial/src/main/java/com/github/mrdynamo/tutorial/util/RegistryHandler.java
+++ tutorial/src/main/java/com/github/mrdynamo/tutorial/util/RegistryHandler.java
@@ -5,8 +5,9 @@ import com.github.mrdynamo.tutorial.blocks.BlockItemBase;
 import com.github.mrdynamo.tutorial.blocks.RubyBlock;
 import com.github.mrdynamo.tutorial.blocks.RubyOre;
 import com.github.mrdynamo.tutorial.items.ItemBase;
+import com.github.mrdynamo.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -25,6 +26,16 @@ public class RegistryHandler {
 
     /*** Items ***/
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    /*** Tools ***/
+    // Damage: 1 + baseDamage(5) + addedDamage(3)
+    // Speed: 4 - attSpd (Cooldown)
+
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () -> new SwordItem(ModItemTier.RUBY, 3, -0.5F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(ModItemTier.RUBY, 3, -0.5F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () -> new ShovelItem(ModItemTier.RUBY, 3, -1.4F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () -> new AxeItem(ModItemTier.RUBY, 3, -1.6F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () -> new HoeItem(ModItemTier.RUBY, -0.5F, new Item.Properties().group(Tutorial.TAB)));
 
     /*** Blocks ***/
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
